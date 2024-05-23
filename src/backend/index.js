b742b716bdf8a917cdb0fd84f0d9e6ee16fff508
synt4ctic <163228692+synt4ctic@users.noreply.github.com>
2024-05-23 22:56:03 +0000

    serve styles over serve-index

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,12 +1,20 @@
 const { exec } = require("child_process");
 const express = require("express");
 const path = require("path");
+const serveIndex = require("serve-index");
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/../web", "index.html"));
 });
+
+// serve entire styles folder
+app.use(
+    "/styles",
+    express.static("../web/styles"),
+    serveIndex("../web/styles", { icons: true })
+);
 
 const PORT = process.env.port || 3142;
 
