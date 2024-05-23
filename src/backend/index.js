74de80ee70796b1afa8efa465ceebd56e5fd772e
synt4ctic <163228692+synt4ctic@users.noreply.github.com>
2024-05-23 22:47:26 +0000

    even more webserver setup :/

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,14 +1,15 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/../web/index.html")
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../web", "index.html"));
 });
 
 const PORT = process.env.port || 3142;
 
 app.listen(PORT, () => {
-    console.log("rpi-radio-app is listening at localhost:3142")
-})
+    console.log("rpi-radio-app is listening at localhost:3142");
+});
