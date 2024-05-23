9dfb88d9124f8c6bd91749384cbe09315e526a34
synt4ctic <163228692+synt4ctic@users.noreply.github.com>
2024-05-23 22:09:11 +0000

    project setup and install script

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
+RPI_RADIO_DIR=$(pwd)
+
 sudo apt-get update
 sudo apt-get install make build-essential libraspberrypi-dev 
+npm install express
+cd && git clone https://github.com/markondej/fm_transmitter
+cd fm_transmitter
+make
