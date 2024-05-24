cca7c54f9e66a40ce2e7249a2860180a7334f094
synt4ctic <163228692+synt4ctic@users.noreply.github.com>
2024-05-24 05:09:10 +0000

    onreadystatechange instead of onload

--- src/web/scripts/post.js
+++ src/web/scripts/post.js
@@ -2,17 +2,10 @@ function req(type, path) {
     var request = new XMLHttpRequest();
     request.open(type || "GET", "./" + (path || ""), true);
 
-    request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE) {
             // Success!
-        } else {
-            // We reached our target server, but it returned an error
-            console.error(
-                "The server returned an error with a status of " +
-                    request.status
-            );
-
-            return null;
+            return request.responseText;
         }
     };
 
