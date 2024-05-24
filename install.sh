10f0d6e8e48cff6199f37f54aa0e9e812e83ba48
synt4ctic <163228692+synt4ctic@users.noreply.github.com>
2024-05-24 01:01:45 +0000

    skeleton of file upload system and other changes

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 RPI_RADIO_DIR=$(pwd)
 
+mkdir uploads
 sudo apt-get update
 sudo apt-get install make build-essential libraspberrypi-dev npm
-sudo npm i express multer serve-index
-cd && git clone https://github.com/markondej/fm_transmitter
-cd fm_transmitter
-make
+sudo npm i express multer serve-index socket.io
+cd
+if test -d ~/fm_transmitter; then
+  echo "FM transmitter directory already present. If this is a mistake, please remove the directory at ~/fm_transmitter."
+fi
+else 
+    git clone https://github.com/markondej/fm_transmitter
+    cd fm_transmitter
+    make
+fi
--- src/backend/index.js
+++ src/backend/index.js
@@ -2,11 +2,19 @@ const { exec } = require("child_process");
 const express = require("express");
 const path = require("path");
 const serveIndex = require("serve-index");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const multer = require("multer");
 
 const app = express();
 
+const server = createServer(app);
+const io = new Server(server);
+
+const upload = multer({ dest: "/uploads" });
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/../web", "index.html"));
+    res.sendFile(path.join(__dirname, "../web", "index.html"));
 });
 
 // serve entire styles folder
@@ -15,6 +23,14 @@ app.use(
     express.static(path.join(__dirname, "../web", "styles")),
     serveIndex(path.join(__dirname, "../web", "styles"), { icons: true })
 );
+
+app.post("/api/upload", (req, res) => {
+    res.send("File uploaded successfully.");
+});
+
+io.on("connection", (socket) => {
+    console.log("New connection");
+});
 
 const PORT = process.env.port || 3142;
 
