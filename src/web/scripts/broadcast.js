2d6139e60697e7c7aaae1a99b878267a3b7c8cdd
synt4ctic <163228692+synt4ctic@users.noreply.github.com>
2024-05-25 05:24:53 +0000

    undo previous fix

--- src/web/scripts/broadcast.js
+++ src/web/scripts/broadcast.js
@@ -1,3 +1,5 @@
+import { socket } from "./main";
+
 function broadcastSong(filename) {
     socket.emit("playSong", filename);
 }
--- src/web/scripts/main.js
+++ src/web/scripts/main.js
@@ -3,3 +3,5 @@ const socket = io();
 setInterval(() => {
     updateList();
 }, 1000);
+
+export { socket };
