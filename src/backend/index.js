e55dc203d6490d63952acd43d716be4b19656a85
synt4ctic <163228692+synt4ctic@users.noreply.github.com>
2024-05-25 05:29:04 +0000

    integrate node:http for server.listen instead of app.listen

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,5 +1,5 @@
 const { exec } = require("child_process");
-const { createServer } = require("http");
+const { createServer } = require("node:http");
 const { Server } = require("socket.io");
 const express = require("express");
 const path = require("path");
@@ -73,6 +73,6 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.port || 3142;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("rpi-radio-app is listening at localhost:3142");
 });
